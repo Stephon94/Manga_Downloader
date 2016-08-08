b7f75126f76764b490a6cf2dcfda9abed8dd24b5
Brian Cole <Brian Cole>
2016-08-08 15:56:49 -0400

    update 8/8/2016 Should now support other Operating Systems

--- Actual Manga Downloader v.2.py
+++ Actual Manga Downloader v.2.py
@@ -9,6 +9,7 @@ print"__________________________________________"
 print"__________________________________________"
 
 website = 'http://www.mangareader.net'
+DIR = os.path.dirname(os.path.dirname(__file__))
 
 def mangaSetup():
     #Gets all the mangas hosted on the site
@@ -43,6 +44,8 @@ def mangaSetup():
 
             mangaName = raw_input("Which manga do you want to download from?").title()
             mangaLink = mangaDict.get(mangaName)
+
+            manga_DIR = os.path.join(DIR, 'Mangas', mangaName)
                     
 
         else:
@@ -50,7 +53,7 @@ def mangaSetup():
             
             print mangaLink
                     
-                    
+            manga_DIR = os.path.join(DIR, 'Mangas', mangaName)        
             
 
     
@@ -65,34 +68,48 @@ def mangaSetup():
         mangaSetup()
     #Checking if User already has a directory for said manga. If yes, change into that directory.
     try:
-        os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper())
+        os.chdir(manga_DIR)
     #If User doesn't have a directory for said manga, one is created then changed into.
     except:
-        os.mkdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper())                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-        os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper())
+        os.mkdir(manga_DIR)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        os.chdir(manga_DIR)
             
 
         
-    '''mangaDownloader()
-    decision = raw_input("Do you want the whole manga?")
-    mangaUrl = mangaDownloader()'''
-    
-    
-    entireManga(mangaLink, mangaHome, mangaName)
+    while True:
+        decision = raw_input("Do you want the whole manga? y or n: ").lower()
+        if decision == "y":
+            entireManga(mangaLink, mangaHome, mangaName, manga_DIR)
+        elif decision == "n":
+            while True:
+                mangaChapter = raw_input("What manga chapter would you like? ")
+                try:
+                    int(mangaChapter)
+                    break
+                    
+                except:
+                    print "Please enter a number"
+                    continue
+        else:
+            continue
 
-def entireManga(mangaLink, mangaHome, mangaName):
+              
+        oneChapter(mangaLink, mangaHome, mangaName, manga_DIR, mangaChapter)
+        break 
+
+def entireManga(mangaLink, mangaHome, mangaName, manga_DIR):
     
     #Going to set manga chapter to 1, since were starting from the beginning
     mangaChapter = 1
     
     #Just like the manga name, checking if you already have the 1st chapter (most likely you won't, but just a failsafe)
     try:
-        os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
+        os.chdir(os.path.join(manga_DIR, str(mangaChapter)))
         
     #If I can't change into the directory, it doesn't exist. So it's created then changed into.
     except:
-        os.mkdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
-        os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
+        os.mkdir(os.path.join(manga_DIR, str(mangaChapter)))
+        os.chdir(os.path.join(manga_DIR, str(mangaChapter)))
         
     #Initializing the variable which will be used for the naming of each manga page
     fileNameNum = 0
@@ -152,10 +169,10 @@ def entireManga(mangaLink, mangaHome, mangaName):
                     #If there is an image, then we know that chapter is downloadable, so a folder is made for that chapter and changed into.
                     if image != None:
                         try:
-                            os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
+                            os.chdir(os.path.join(manga_DIR, str(mangaChapter)))
                         except:
-                            os.mkdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
-                            os.chdir("C:/Users/Brian/Desktop/Mangas/"+mangaName.upper()+"/"+str(mangaChapter))
+                            os.mkdir(os.path.join(manga_DIR, str(mangaChapter)))
+                            os.path.join(manga_DIR, str(mangaChapter))
 
                         #File number reset because this is a new chapter
                         fileNameNum = 0
@@ -165,5 +182,68 @@ def entireManga(mangaLink, mangaHome, mangaName):
     #Just printing out any errors without actually breaking the code.               
     except Exception,e:
         print str(e)
+
+
+
+def oneChapter(mangaLink, mangaHome, mangaName, manga_DIR, mangaChapter):
+    #Just like the manga name, checking if you already have the 1st chapter (most likely you won't, but just a failsafe)
+    try:
+        os.chdir(os.path.join(manga_DIR, str(mangaChapter)))
+        print "Hi"
+        
+    #If I can't change into the directory, it doesn't exist. So it's created then changed into.
+    except:
+        os.mkdir(os.path.join(manga_DIR, str(mangaChapter)))
+        os.chdir(os.path.join(manga_DIR, str(mangaChapter)))
+        
+    #Initializing the variable which will be used for the naming of each manga page
+    fileNameNum = 0
+    print fileNameNum
+    #initializing a variable that holds what would be considered the "next page" of the manga
+    nextPage = mangaLink+"/"+str(mangaChapter)
+    print "Hi"
+
+    #Initializes a variable that holds the beginning of the manga, then passes it through Beautiful Soup for parsing   
+    mangaBegins = mangaHome+'/'+str(mangaChapter)
+    
+    soup = BeautifulSoup(look.urlopen(mangaBegins).read(), "html.parser")
+    
+    
+    #Initializes a variable that holds the chapter number for each page being downloaded   
+    chapterNum = int(nextPage.split("/")[2])
+    
+    #Starts a loop so that each page will be downloaded.   
+    while chapterNum == int(mangaChapter):
+        #Starts another loop that finds the manga pages image link & sets it a variable
+        for a in soup.find_all('img', src=True):
+                    if len(a['src']) > 0:
+                        image = a['src']
+                        
+                        #Taking the same variable for each manga page and making sure it adds one for each page downloaded.
+                        fileNameNum += 1
+                        
+                        #Initializes variable for the actual name of the downloaded manga page
+                        fileName = '0' + str(fileNameNum) +'.jpg'
+                        
+                        #This creates the file, saves the manga page as this file, then closes the file.
+                        f = open(fileName,'wb')
+                        f.write(requests.get(image).content)
+                        f.close()
+                        
+                        #Just letting the user know their progress, showing what's been downloaded so far
+                        print nextPage, 'Successfully downloaded!'
+                    
+        #Parses the the html look for the specific link for the next page and sets it to the recently made nextPage variable            
+        for span in soup.find_all('span', {'class' : "next"}):
+            nextPage = span.a['href']
+
+            #Checking each page to know when the next chapter starts.
+            chapterNum = int(nextPage.split("/")[2])
+            if chapterNum != int(mangaChapter) + 1:
+                nextPage = nextPage
+                soup = BeautifulSoup(look.urlopen(website+nextPage).read(),"html.parser")
+            else:
+                print "Enjoy your manga chapter!"
+                mangaSetup()
         
 mangaSetup()
